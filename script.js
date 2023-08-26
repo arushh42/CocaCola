c1bc6a6b0cdd128d4ded0b68c69ac3fe1e317ef0
Arush Karekar <arushkarekar@Arushs-MacBook-Pro.local>
2023-08-26 16:30:51 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,25 @@
+var swiper = new Swiper('.swiper-container', {
+        
+    effect: 'coverflow',
+    grabCursor: true,
+    centeredSlides: true,
+    slidesPerView: 'auto',
+    coverflow: {
+        rotate: 0,
+        stretch: 0,
+        depth: 200,
+        modifier: 1,
+        slideShadows : true
+    },
+    pagination: {el: '.swiper-pagination'},
+    },
+
+    loop= true,
+    );
+
+function toggleMenu(){
+    const menuToggle = document.querySelector('.toggle');
+    const navigation = document.querySelector('.navigation');
+    menuToggle.classList.toggle('active');
+    navigation.classList.toggle('active');
+}
